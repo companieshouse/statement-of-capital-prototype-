8f32da39ebcb8296d4224bd4998fd7da0cf8d0b7
Rebecca Hazell <rhazell@companieshouse.gov.uk>
2021-03-05 12:51:21 +0000

    Adding to SH01 journey

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Statement of Capital',
+  serviceName: 'Notify Companies House of a change to the share capital',
 
   // Default port that prototype runs on
   port: '3000',
