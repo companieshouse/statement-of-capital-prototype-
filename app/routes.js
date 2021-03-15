94254b6c90b4e3f41df7479ce626a1d6a8a5787b
Rebecca Hazell <rhazell@companieshouse.gov.uk>
2021-03-15 10:07:52 +0000

    Longer, more detailed statement of capital added

--- app/routes.js
+++ app/routes.js
@@ -2,20 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
-// Branching
-router.post('/examples/branching/over-18-answer', function (req, res) {
-  // Get the answer from session data
-  // The name between the quotes is the same as the 'name' attribute on the input elements
-  // However in JavaScript we can't use hyphens in variable names
-
-  const over18 = req.session.data['over-18']
-
-  if (over18 === 'false') {
-    res.redirect('/docs/examples/branching/under-18')
-  } else {
-    res.redirect('/docs/examples/branching/over-18')
-  }
-})
 
 // Branching
 router.get('/change-answer', function (req, res) {
@@ -26,8 +12,23 @@ router.get('/change-answer', function (req, res) {
   const change = req.session.data['form']
   if (change === 'SH01') {
     res.redirect('/user-journeys/SH01/date-of-allotment')
+  } else if (change === 'SH06') {
+    res.redirect('/user-journeys/SH01/date-of-allotment')
   } else {
-    res.redirect('/docs/examples/branching/over-18')
+    res.redirect('/user-journeys/not-developed-yet')
+  }
+})
+
+router.get('/soc-answer', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  const soc = req.session.data['statement-of-capital']
+  if (soc === 'yes') {
+    res.redirect('/confirmation-page')
+  } else {
+    res.redirect('/template-pages/default-pages/change')
   }
 })
 
