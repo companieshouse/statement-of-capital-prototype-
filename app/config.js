396a61e5874ed152caa0c197b517867d1372b2fa
Rebecca Hazell <rhazell@companieshouse.gov.uk>
2021-04-27 09:58:50 +0100

    Service name updated after naming workshop

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Update share capital',
+  serviceName: 'Update a companys share capital information',
 
   // Default port that prototype runs on
   port: '3000',
