7448c24774b2a85fdf1ab2ebcf1bb2aa6d41f469
Rebecca Hazell <rhazell@companieshouse.gov.uk>
2021-04-30 18:55:30 +0100

    Refactoring code

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Update a companys share capital information',
+  serviceName: 'Update a company\'s share capital information',
 
   // Default port that prototype runs on
   port: '3000',
