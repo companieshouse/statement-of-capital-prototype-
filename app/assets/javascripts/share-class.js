2fc298f89b003cb79728d1a449fdfc867a056ac5
Jason <jdeakin@companieshouse.gov.uk>
2021-05-21 07:13:00 +0100

    Added accessible autocomplete to share type page

---
+++ app/assets/javascripts/share-class.js
@@ -0,0 +1,22 @@
+var classes = [
+	"Ordinary",
+	"Ordinary A",
+	"Ordinary B",
+	"Ordinary C",
+	"Ordinary D",
+	"Ordinary E",
+	"Ordinary F",
+	"Ordinary G",
+	"Ordinary H",
+	"Ordinary I",
+	"Ordinary J",
+	"Ordinary K",
+	"Deferred ordinary",
+	"Preference",
+	"Redeemable",
+	"Redeemable preference",
+  ]
+  var element = document.querySelector('#class')
+  var id = 'autocomplete-default'
+  accessibleAutocomplete({element: element, id: id, source: classes})
+  
